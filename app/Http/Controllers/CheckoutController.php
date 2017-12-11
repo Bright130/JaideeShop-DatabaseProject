0e223ac60b87b5f99e2ea79cb1c8ea1164c3ecfb
REMI\P'sycho-Remi <peatza93@hotmail.com>
2017-12-12 05:57:09 +0700

    show orderrr

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -10,6 +10,8 @@ use App\Product;
 use App\Productimage;
 use App\Shippingtype;
 use App\Cart;
+use App\newSeller;
+use App\Shop;
 
 class CheckoutController extends Controller
 {
@@ -26,12 +28,29 @@ class CheckoutController extends Controller
   }
   public function postShip(Request $request)
   {
+    $bname = Auth::User()->buyername;
+    $bsurname = Auth::User()->buyersurname;
+    $baddress = Auth::User()->buyeraddress;
      $data = $request->all();
      $decoded = json_decode($request->name, true);
-     dd($decoded);
     $shiptype = Shippingtype::where('shippingtype','LIKE',"{$request->input('shiptype')}%")->get();
-    // dd($shiptype);
-    return view('order',compact('shiptype'));
+     $owners = [];
+     $owner = [];
+     $surname = [];
+    foreach ($decoded as $decode) { $shopown = Product::select('shopid')->where('productid','=',$decode["id"])->get();
+     foreach ($shopown as $shopo) { $sellerown = Shop::where('shopid','=',$shopo->shopid)->get();
+       foreach ($sellerown as $sellero) { $owners[] = newSeller::where('id','=',$sellero->sellerid)->get();
+         # code...
+       }
+     }
+      // $owner[] = newSeller::where('id','=',$sellerown)->get();
+    }
+    foreach ($owners as $ow) { foreach ($ow as $o) { $owner[] = $o->sellername; $surname[] = $o->sellersurname;
+      # code...
+    }
+      # code...
+    }
+    return view('order',['decoded'=>$decoded,'shiptype'=>$shiptype,'bname'=>$bname,'bsurname'=>$bsurname,'baddress'=>$baddress,'owner'=>$owner,'surname'=>$surname]);
   }
   public function orders()
   {
